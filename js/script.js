eb4fe245f6e4f51f0508a8c682f9d03a083dfa0b
xlgp <zhang15733@sina.cn>
2017-04-03 10:30:16 +0800

    Site updated: 2017-04-03 10:30:16

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 (function ($, window) {
- 
+
     var App = App || {
         init: function () {
             $('.js-toggle-search').on('click', function () {
                 $('.js-search').toggleClass('is-visible');
             });
- 
+
             $('.js-next a').on('click', function (e) {
                 $(infinite_scroll.contentSelector).infinitescroll(infinite_scroll);
- 
+
                 var $body = $('body');
- 
+
                 $body.scrollTop($body.scrollTop() - 1);
- 
+
                 e.preventDefault();
             })
- 
+
             $(window).keydown(function (event) {
                 if (event.keyCode == 27) {
                     if ($('.js-search').attr('class').indexOf('is-visible') > 0) {
@@ -23,25 +23,25 @@
                     }
                 }
             });
- 
+
             $('.js-search .text-input').keydown(function (event) {
                 if (event.keyCode == 13) {
-                    location.href = 'https://www.google.com/search?q=site:yumemor.com ' + $(this).val();
+                    location.href = 'https://www.google.com/search?q=site:xlgp.github.io ' + $(this).val();
                     return false;
                 }
             })
         }
     };
- 
+
 
     $(App.init);
 
     $(function(){
- 
+
         var navToggle = $('#nav-toggle'),
             nav = $('nav'),
             navLinks = nav.find('a');
- 
+
         navToggle.on('click', function () {
             navToggle.toggleClass('active');
             nav.toggleClass('open');
@@ -51,34 +51,34 @@
             navToggle.toggleClass('active');
             nav.toggleClass('open');
         });
- 
+
         $(document).on('click', function () {
             if (nav.hasClass('open')) {
                 navToggle.toggleClass('active');
                 nav.toggleClass('open');
             }
         });
- 
+
         $('.btn-slide').click(function () {
             $('#panel').slideToggle("slow");
             $(this).toggleClass("active");
             return false;
         });
- 
+
         $(window).scroll(function () {
             var header = $('header');
- 
+
             if ($(this).scrollTop() > 1) {
                 header.addClass("scrolled");
             } else {
                 header.removeClass("scrolled");
             }
         });
- 
+
         $("#social-share").click(function () {
             $("#social").toggleClass("visible").slideToggle(200);
         });
- 
+
         if ($('.welcome')[0]) {
             $('.author-info').hide();
             $('span.info-edit').click(function () {
@@ -88,8 +88,8 @@
 
         var bannerNode = $('.top-image');
         if(bannerNode.data('enable')){
-            // var url = '/banner/'+parseInt((Math.random() * 2) + 1)+'.jpg';
-            var url = 'http://img.infinitynewtab.com/wallpaper/'+Math.floor((Math.random()*3000)+1)+'.jpg';
+            var url = '/banner/'+parseInt((Math.random() * 2) + 1)+'.jpg';
+            // var url2 = 'http://img.infinitynewtab.com/wallpaper/'+Math.floor((Math.random()*3000)+1)+'.jpg';
             bannerNode.attr('style','background-image:url('+url+')');
         }
     })
